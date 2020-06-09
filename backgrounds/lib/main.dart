442720b49fd0dac144e7c81c91088ea7a0b5d7b3
michael rondon <mrondon72@gmail.com>
2020-06-10 01:17:44 +0200

    13. Header Cuadrado

---
+++ backgrounds/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'src/pages/headers_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños App',
+      home: HeadersPage()
+    );
+ 
+  }
+
+}
---
+++ backgrounds/lib/src/widgets/headers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Container(
+        height: 300.0,
+        color: Color(0xff615AAB)
+    );
+  
+  }
+
+}
