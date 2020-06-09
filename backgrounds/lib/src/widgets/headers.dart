07190a180ad524ebd80d99ab2b83f611463abe15
michael rondon <mrondon72@gmail.com>
2020-06-10 01:46:33 +0200

    17. Header Diagonal

--- backgrounds/lib/src/widgets/headers.dart
+++ backgrounds/lib/src/widgets/headers.dart
@@ -40,12 +40,44 @@ class HeaderDiagonal extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return Container(
-      height: 300.0,
-      color: Color(0xff615AAB)
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter()
+      ),
     );
   
   }
 
 }
 
+class _HeaderDiagonalPainter extends CustomPainter {
 
+  @override
+  void paint(Canvas canvas, Size size) {
+    
+    final paint = new Paint();
+
+    // Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill; // .fill .stroke
+    paint.strokeWidth = 2;
+
+    final path = new Path();
+
+    // Dibujar con el path y el paint
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
