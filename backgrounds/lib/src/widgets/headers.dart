57fa2c8e2fc56a1da0e521f7e1da6529847fea1d
michael rondon <mrondon72@gmail.com>
2020-06-10 01:22:20 +0200

    14. Header Circular

--- backgrounds/lib/src/widgets/headers.dart
+++ backgrounds/lib/src/widgets/headers.dart
@@ -13,3 +13,23 @@ class HeaderCuadrado extends StatelessWidget {
   }
 
 }
+
+class HeaderBordersRendondeados extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Container(
+        height: 300.0,
+        decoration: BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(70),
+            bottomRight: Radius.circular(70)
+          )
+        )
+    );
+  
+  }
+
+}
