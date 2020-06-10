309db26c433296b1a4c7c76e1ab0e1233d874db9
michael rondon <mrondon72@gmail.com>
2020-06-10 17:53:33 +0200

    31. Curves

--- animaciones/lib/src/pages/animaciones_page.dart
+++ animaciones/lib/src/pages/animaciones_page.dart
@@ -35,13 +35,16 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProv
       duration: Duration(milliseconds: 4000)
     );
 
-    rotacion = Tween(begin: 0.0, end: 2 * Math.pi).animate(controller);
+    rotacion = Tween(begin: 0.0, end: 2 * Math.pi).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeOut)
+    );
 
     controller.addListener(() {
       print('Status: ${ controller.status }');
 
       if ( controller.status == AnimationStatus.completed ) {
-        controller.reverse();
+        // controller.reverse();
+        controller.reset();
       }
     });
 
