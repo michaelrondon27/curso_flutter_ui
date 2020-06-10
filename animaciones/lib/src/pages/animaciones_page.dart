e8a680d47eb7926eb4e4772aae9ef7a94938c024
michael rondon <mrondon72@gmail.com>
2020-06-10 17:20:34 +0200

    27. Preparando la pantalla para animar

---
+++ animaciones/lib/src/pages/animaciones_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AnimacionesPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      )
+    );
+  
+  }
+
+}
+
+class CuadradoAnimado extends StatefulWidget {
+
+  @override
+  _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
+
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return _Rectangulo();
+  
+  }
+
+}
+
+class _Rectangulo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.blue
+      )
+    );
+
+  }
+
+}
