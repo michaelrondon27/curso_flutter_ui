468c07a63c9de74a981f6b62ac5610fa587e34f0
michael rondon <mrondon72@gmail.com>
2020-06-11 00:13:17 +0200

    33. Animar en intervalos de tiempo

--- animaciones/lib/src/pages/animaciones_page.dart
+++ animaciones/lib/src/pages/animaciones_page.dart
@@ -40,7 +40,12 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProv
       CurvedAnimation(parent: controller, curve: Curves.easeOut)
     );
 
-    opacidad = Tween(begin: 0.1, end: 1.0).animate(controller);
+    opacidad = Tween(begin: 0.1, end: 1.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0, 0.25, curve: Curves.easeOut)
+      )
+    );
 
     controller.addListener(() {
       print('Status: ${ controller.status }');
