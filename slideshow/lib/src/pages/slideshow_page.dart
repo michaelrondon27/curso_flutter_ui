fb43286106083a2f3c3add7d3c8a4a12659c0bb1
michael rondon <mrondon72@gmail.com>
2020-06-13 00:53:10 +0200

    63. Diseño de los puntos indicadores

--- slideshow/lib/src/pages/slideshow_page.dart
+++ slideshow/lib/src/pages/slideshow_page.dart
@@ -10,7 +10,14 @@ class SlideShowPage extends StatelessWidget {
   
     return Scaffold(
       body: Center(
-        child: _Slides()
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: _Slides()
+            ),
+            _Dots()
+          ],
+        )
       )
     );
   
@@ -53,6 +60,47 @@ class _Slide extends StatelessWidget {
       child: SvgPicture.asset(svg)
     );
 
+  }
+
+}
+
+class _Dots extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _Dot(),
+          _Dot(),
+          _Dot()
+        ],
+      )
+    );
+
+  }
+
+}
+
+class _Dot extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      width: 12,
+      height: 12,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle
+      )
+    );
+  
   }
 
 }
