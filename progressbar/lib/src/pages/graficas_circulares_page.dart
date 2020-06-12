ae5fb471cf7fa6849218401d30f87d1f53170632
michael rondon <mrondon72@gmail.com>
2020-06-12 02:03:22 +0200

    53. Jugando con nuestro RadialProgress

--- progressbar/lib/src/pages/graficas_circulares_page.dart
+++ progressbar/lib/src/pages/graficas_circulares_page.dart
@@ -29,21 +29,57 @@ class _GraficasCircularesPageState extends State<GraficasCircularesPage> {
           });
         },
       ),
-      body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          child: RadialProgress(
-            porcentaje: porcentaje,
-            colorPrimario: Colors.purple,
-            colorSecundario: Colors.grey,
-            grosorPrimario: 10,
-            grosorSecundario: 4
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              CustomRadialProgress(porcentaje: porcentaje, color: Colors.blue),
+              CustomRadialProgress(porcentaje: porcentaje, color: Colors.red)
+            ],
           ),
-        )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              CustomRadialProgress(porcentaje: porcentaje, color: Colors.pink),
+              CustomRadialProgress(porcentaje: porcentaje, color: Colors.purple)
+            ],
+          )
+        ],
       )
     );
   
+  }
+
+}
+
+class CustomRadialProgress extends StatelessWidget {
+
+  final Color color;
+
+  const CustomRadialProgress({
+    @required this.porcentaje,
+    @required this.color
+  });
+
+  final double porcentaje;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: 180,
+      height: 180,
+      child: RadialProgress(
+        porcentaje: porcentaje,
+        colorPrimario: color,
+        colorSecundario: Colors.grey,
+        grosorPrimario: 10,
+        grosorSecundario: 4
+      ),
+    );
+
   }
 
 }
