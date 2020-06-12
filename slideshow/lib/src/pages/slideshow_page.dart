e404a2c06859f4712c52a5b356109f1b9bea5326
michael rondon <mrondon72@gmail.com>
2020-06-13 01:01:13 +0200

    64. PageViewController - Escuchar cambios de paginas

--- slideshow/lib/src/pages/slideshow_page.dart
+++ slideshow/lib/src/pages/slideshow_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -25,13 +27,38 @@ class SlideShowPage extends StatelessWidget {
 
 }
 
-class _Slides extends StatelessWidget {
+class _Slides extends StatefulWidget {
   
+  @override
+  __SlidesState createState() => __SlidesState();
+
+}
+
+class __SlidesState extends State<_Slides> {
+
+  final pageViewController = new PageController();
+
+  @override
+  void initState() {
+    super.initState();
+  
+    pageViewController.addListener(() {
+      print('Pagina actual: ${ pageViewController.page }');
+    });
+  }
+
+  @override
+  void dispose() { 
+    pageViewController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
   
     return Container(
       child: PageView(
+        controller: pageViewController,
         children: <Widget>[
           _Slide('assets/svgs/slide-1.svg'),
           _Slide('assets/svgs/slide-2.svg'),
@@ -40,8 +67,8 @@ class _Slides extends StatelessWidget {
       )
     );
   
-  }
 
+  }
 }
 
 class _Slide extends StatelessWidget {
@@ -75,9 +102,9 @@ class _Dots extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          _Dot(),
-          _Dot(),
-          _Dot()
+          _Dot(0),
+          _Dot(1),
+          _Dot(2)
         ],
       )
     );
@@ -87,6 +114,10 @@ class _Dots extends StatelessWidget {
 }
 
 class _Dot extends StatelessWidget {
+
+  final int index;
+
+  const _Dot(this.index);
 
   @override
   Widget build(BuildContext context) {
