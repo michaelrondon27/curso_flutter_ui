191054aa8aee89b8652eec52c7464a2154ca6994
michael rondon <mrondon72@gmail.com>
2020-06-13 17:14:55 +0200

    95. IconHeader

---
+++ emergency/lib/src/pages/emergency_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/header.dart';
+
+class EmergencyPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      body: IconHeader()
+    );
+  
+  }
+
+}
