0f648398d5526ca3212564041af73f8f1e38ce0f
michael rondon <mrondon72@gmail.com>
2020-06-13 15:48:42 +0200

    81. Pinterest menu

--- pinterest/lib/src/pages/pinterest_page.dart
+++ pinterest/lib/src/pages/pinterest_page.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+import '../widget/pinterest_menu.dart';
+
 class PinterestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
   
     return Scaffold(
-      body: Center(
-        child: PinterestGrid()
-      )
+      body: PinterestMenu()
     );
   
   }
