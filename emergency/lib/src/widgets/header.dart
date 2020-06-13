bae36d9de6bc9e402eca0ed969f4bc0a1b577f12
michael rondon <mrondon72@gmail.com>
2020-06-13 17:24:31 +0200

    96. Iconos y textos de mi IconHeader

--- emergency/lib/src/widgets/header.dart
+++ emergency/lib/src/widgets/header.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class IconHeader extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final Color colorBlanco = Colors.white.withOpacity(0.7);
   
+    return Stack(
+      children: <Widget>[
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon( FontAwesomeIcons.plus, size: 250, color: Colors.white.withOpacity(0.2) )
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(height: 80, width: double.infinity),
+            Text('Haz solicitado', style: TextStyle( fontSize: 20, color: colorBlanco )),
+            SizedBox(height: 20),
+            Text('Asistencia Médica', style: TextStyle( fontSize: 25, color: colorBlanco, fontWeight: FontWeight.bold )),
+            SizedBox(height: 20),
+            FaIcon( FontAwesomeIcons.plus, size: 80, color: Colors.white )
+          ],
+        )
+      ],
+    );
+  
+  }
+
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
     return Container(
       width: double.infinity,
       height: 300,
@@ -20,7 +54,7 @@ class IconHeader extends StatelessWidget {
         )
       )   
     );
-  
+
   }
 
 }
