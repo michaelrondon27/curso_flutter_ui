91a0ebf889b701883f7e0077e55c333040fcd2b3
michael rondon <mrondon72@gmail.com>
2020-06-13 15:40:49 +0200

    80. Inicio de proyecto - Pinterest

---
+++ pinterest/lib/src/pages/pinterest_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class PinterestPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      body: Center(
+        child: PinterestGrid()
+      )
+    );
+  
+  }
+
+}
+
+class PinterestGrid extends StatelessWidget {
+
+  final List<int> items = List.generate(200, (i) => i);
+
+  @override  
+  Widget build(BuildContext context) {
+
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 4,
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) => _PinterestItem( index ),
+      staggeredTileBuilder: (int index) => StaggeredTile.count(2, index.isEven ? 2 : 3),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  
+  }
+
+}
+
+class _PinterestItem extends StatelessWidget {
+
+  final int index;
+
+  const _PinterestItem(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all( Radius.circular(30) )
+      ),
+      child: Center(
+        child: CircleAvatar(
+        backgroundColor: Colors.white,
+        child: Text('$index'),
+      ),
+    ));
+
+  }
+}
