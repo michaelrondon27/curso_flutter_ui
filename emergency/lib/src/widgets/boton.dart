d298c387332230ab1d2e12b105cea9dc0852234b
michael rondon <mrondon72@gmail.com>
2020-06-14 16:26:49 +0200

    99. Informacion dentro del boton

--- emergency/lib/src/widgets/boton.dart
+++ emergency/lib/src/widgets/boton.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class Boton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
 
-    return _BotonBackground();
+    return Stack(
+      children: <Widget>[
+        _BotonBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 140, width: 40),
+            FaIcon( FontAwesomeIcons.carCrash, color: Colors.white, size: 40 ),
+            SizedBox(width: 20),
+            Expanded(child: Text('Motor Accident', style: TextStyle(color: Colors.white, fontSize: 18))),
+            FaIcon( FontAwesomeIcons.chevronRight, color: Colors.white ),
+            SizedBox(width: 40),
+          ],
+        )
+      ],
+    );
 
   }
 
@@ -17,6 +34,18 @@ class _BotonBackground extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: -20,
+              top: -20,
+              child: FaIcon( FontAwesomeIcons.carCrash, size: 150, color: Colors.white.withOpacity(0.2) ),
+            )
+          ],
+        ),
+      ),
       width: double.infinity,
       height: 100,
       margin: EdgeInsets.all(20),
