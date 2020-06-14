da45af10aaaf67eb8db5ff79050037b56036fdbc
michael rondon <mrondon72@gmail.com>
2020-06-14 23:01:40 +0200

    111. CustomScrollView y slivers

--- sliver/lib/src/pages/sliver_list_page.dart
+++ sliver/lib/src/pages/sliver_list_page.dart
@@ -6,7 +6,41 @@ class SliverListPage extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return Scaffold(
-      body: _ListaTareas()
+      body: _MainScroll()
+    );
+  
+  }
+
+}
+
+class _MainScroll extends StatelessWidget {
+
+  final items = [
+    _ListItem( 'Orange', Color(0xffF08F66) ),
+    _ListItem( 'Family', Color(0xffF2A38A) ),
+    _ListItem( 'Subscriptions', Color(0xffF7CDD5) ),
+    _ListItem( 'Books', Color(0xffFCEBAF) ),
+    _ListItem( 'Orange', Color(0xffF08F66) ),
+    _ListItem( 'Family', Color(0xffF2A38A) ),
+    _ListItem( 'Subscriptions', Color(0xffF7CDD5) ),
+    _ListItem( 'Books', Color(0xffFCEBAF) ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          floating: true,
+          elevation: 0,
+          backgroundColor: Colors.red,
+          title: Text('Hola Mundo')
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(items)
+        )
+      ],
     );
   
   }
