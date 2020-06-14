12dedc39e9e3ed08aaafb13ce831b3551224e1bc
michael rondon <mrondon72@gmail.com>
2020-06-14 22:44:21 +0200

    109. Diseño basico de la aplicacion

--- sliver/lib/src/pages/sliver_list_page.dart
+++ sliver/lib/src/pages/sliver_list_page.dart
@@ -6,11 +6,81 @@ class SliverListPage extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo')
-      )
+      body: _Titulo()
     );
   
+  }
+
+}
+
+class _Titulo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 30),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text('New', style: TextStyle( color: Color(0xff532128), fontSize: 50 )),
+        ),
+        Stack(
+          children: <Widget>[
+            SizedBox(width: 100),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 150,
+                height: 8,
+                decoration: BoxDecoration(
+                  color: Color(0xffF7CDD5)
+                )
+              ),
+            ),
+            Container(
+              child: Text('List', style: TextStyle( color: Color(0xffD93A30), fontSize: 50, fontWeight: FontWeight.bold )),
+            )
+          ],
+        )
+      ],
+    );
+  
+  }
+
+}
+
+class _ListaTareas extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) => _ListItem()
+    );
+  
+  }
+
+}
+
+class _ListItem extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Text('Orange', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)),
+      padding: EdgeInsets.all(30),
+      alignment: Alignment.centerLeft,
+      height: 130,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(30)
+      )
+    );
+
   }
 
 }
