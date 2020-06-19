e1cf51ac26f0a161d9bfefa4d28c54ae17433c37
michael rondon <mrondon72@gmail.com>
2020-06-19 02:22:50 +0200

    157. Sombra de nuestro zapato

--- shoes/lib/src/widgets/zapato_size.dart
+++ shoes/lib/src/widgets/zapato_size.dart
@@ -35,10 +35,38 @@ class _ZapatoConSombra extends StatelessWidget {
       padding: EdgeInsets.all(50),
       child: Stack(
         children: <Widget>[
+          Positioned(
+            bottom: 20,
+            right: 0,
+            child: _ZapatoSombra()
+          ),
           Image(
             image: AssetImage('assets/imgs/azul.png'),
           )
         ],
+      ),
+    );
+  
+  }
+
+}
+
+class _ZapatoSombra extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        width: 230,
+        height: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: [
+            BoxShadow( color: Color(0xffEAA14E), blurRadius: 40 )
+          ]
+        ),      
       ),
     );
   
