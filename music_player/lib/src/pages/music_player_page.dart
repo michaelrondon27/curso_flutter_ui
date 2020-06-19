c4fefa01d4a36081f0e96570256385aeafcf5817
michael rondon <mrondon72@gmail.com>
2020-06-19 19:56:18 +0200

    176. Imagen del disco

--- music_player/lib/src/pages/music_player_page.dart
+++ music_player/lib/src/pages/music_player_page.dart
@@ -10,9 +10,79 @@ class MusicPlayerPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          CustomAppBar()
+          CustomAppBar(),
+          ImagenDiscoDuracion()
         ]
       )
+    );
+  
+  }
+
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Row(
+        children: <Widget>[
+          ImagenDisco()
+        ],
+      )
+    );
+  
+  }
+
+}
+
+class ImagenDisco extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200),
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage('assets/No Guidance.png')
+            ),
+            Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(100)
+              ),
+            ),
+            Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                color: Color(0xff1C1C25),
+                borderRadius: BorderRadius.circular(100)
+              ),
+            )
+          ],
+        )
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff400321),
+            Color(0xff5D44EC)
+          ]
+        )
+      ),
     );
   
   }
