8570edc9ecedc29019725d6e3d075af9cc708f73
michael rondon <mrondon72@gmail.com>
2020-06-19 21:33:53 +0200

    183. Determinar la duracion de la cancion

--- music_player/lib/src/models/audioplayer_model.dart
+++ music_player/lib/src/models/audioplayer_model.dart
@@ -3,11 +3,52 @@ import 'package:flutter/material.dart';
 class AudioPlayerModel with ChangeNotifier {
 
   bool _playing = false;
+  Duration _songDuration = new Duration(milliseconds: 0);
+  Duration _current = new Duration(milliseconds: 0);
   AnimationController _controller;
+
+  bool get playing => this._playing;
+  set playing( bool valor ) {
+    this._playing = valor;
+    notifyListeners();
+  }
+
+  Duration get songDuration => this._songDuration;
+  set songDuration( Duration valor ) {
+    this._songDuration = valor;
+    notifyListeners();
+  }
+
+  Duration get current => this._current;
+  set current( Duration valor ) {
+    this._current = valor;
+    notifyListeners();
+  }
 
   AnimationController get controller => this._controller;
   set controller( AnimationController valor ) {
     this._controller = valor;
+  }
+
+  double get porcentaje => (this._songDuration.inSeconds > 0) 
+                              ? this._current.inSeconds / this._songDuration.inSeconds
+                              : 0;
+
+  String get songTotalDuration => this.printDuration( this._songDuration );
+
+  String get currentSecond => this.printDuration( this._current );
+
+  String printDuration( Duration duration ) {
+
+    String twoDigits( int n ) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitsMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitsSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitsMinutes:$twoDigitsSeconds";
+
   }
 
 }
